cd6f75442e0c7f59a05a54e8eb68e12694fb7fd9
garikdallari <garikdallari@gmail.com>
2021-09-02 11:43:36 +0300

    Updates

--- static/js/main.efba4978.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),r=n.n(c),o=n(3),a=n.n(o),i=(n(8),n(9),n(0));var s=function(){return Object(i.jsx)(i.Fragment,{})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(s,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.efba4978.chunk.js.map
